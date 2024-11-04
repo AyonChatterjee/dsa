522c9bfb5adcd67d5e6582ee93c7f198c3cbe190
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2024-11-05 00:00:04 +0530

    Reverse a LinkedList

---
+++ LinkedList/ReverseList.java
@@ -0,0 +1,16 @@
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null , curr = head ;
+
+        while(curr != null) {
+          ListNode next = curr.next ;
+
+            curr.next = prev ; 
+            prev = curr ;
+            curr = next ;
+        }
+
+        return prev ;
+
+    }
+}
