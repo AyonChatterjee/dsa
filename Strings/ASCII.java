46bfd910a841259e84142dc75cdaa861f0a15376
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2024-11-08 10:06:55 +0530

    How to calculate ASCII value

---
+++ Strings/ASCII.java
@@ -0,0 +1,10 @@
+public class ASCII {
+    private static void findASCII(char ch) {
+        int ascii = ch;
+        System.out.println("The ASCII value is: " + ascii);
+    }
+
+    public static void main(String[] args) {
+        findASCII('A');
+    }
+}
