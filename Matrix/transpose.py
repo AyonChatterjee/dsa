4e5cdfb23a18977d53ab6e2c51932d9d78040c95
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2024-11-26 16:06:47 +0530

    Transpose of a matrix

---
+++ Matrix/transpose.py
@@ -0,0 +1,30 @@
+N = 4 
+
+
+def transpose(A , B):
+  
+
+    for i in range(N):
+        for j in range(N):
+           B[i][j] = A[j][i]
+
+
+
+# driver code 
+A = [ [1, 1, 1, 1], 
+ [2, 2, 2, 2], 
+ [3, 3, 3, 3], 
+ [4, 4, 4, 4]] 
+ 
+B = A[:][:]
+
+transpose(A , B)
+
+for i  in range(N):
+    for j in range(N):
+        print(B[i][j] , " ", end = '')
+    print()
+        
+
+
+          
