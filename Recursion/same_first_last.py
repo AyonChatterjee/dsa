3f61b020be4ced747575064d29d731d18f522383
Ayon Chatterjee <chatterjeeayon542@gmail.com>
2024-12-02 11:52:55 +0530

    Recursive solution to count substrings with same first and last characters

--- Recursion/same_first_last.py
+++ Recursion/same_first_last.py
@@ -1,20 +1,31 @@
-def checkEquality(s):
-    return (ord(s[0]) == ord(s[len(s) - 1]))
+def countSubstringWithEqualsEnds(s , i , j , n):
 
-def countSubstringWithEqualsEnds(s):
-    res= 0 
-    n = len(s)
-    ans = []
+    # base case
+   if(n == 1):
+      if s[i : j + 1] not in ans:
+       ans.append(s[i : j + 1])
+      return 1
+   
+   if(n <= 0):
+      return 0
+   
 
-    for i in range(n):
-        for j in range(1 , n - i + 1):
-            if(checkEquality(s[i : i + j])):
-                res += 1
-                ans.append(s[i : i + j])
+   res = (countSubstringWithEqualsEnds(s , i + 1 , j , n - 1) 
+       + countSubstringWithEqualsEnds(s , i , j - 1, n - 1)
+       - countSubstringWithEqualsEnds(s , i + 1 , j - 1 , n - 2)) 
+   
 
-    print(f"The Possible strings are : {ans}")            
-    return res 
+   if(s[i] == s[j]) and s[i : j + 1] not in ans:
+       res+= 1
+       ans.append(s[i : j + 1])
+
+   return res  
 
 
 s = "abcab"
-print(countSubstringWithEqualsEnds(s))
+n = len(s)
+ans = []
+
+print(countSubstringWithEqualsEnds(s , 0 , n - 1 , n))
+
+print(f"The combinations are : {ans}")
